edc6aa36d84af8a1421bac3502dc9154576407d1
Anton Pozdnyakov <ap@sqvr.net>
2023-04-06 10:36:23 +0200

    Update messages info

--- messages.py
+++ messages.py
@@ -1,10 +1,11 @@
 def get_messages() -> dict:
     """Словарь с заготовками сообщений пользователю"""
     messages = {
-        'FindUrl': 'Найдена ссылка',
+        'FindUrl': 'Найдена ссылка, пытаемся извлечь видео...',
         'NonUrlMessage': 'Не разгляжу ссылку, обычно это `http...`',
-        'SuccessfulDownload': 'Успешно скачали видео... Ожидайте.',
+        'SuccessfulDownload': 'Успешно скачали видео... Ожидайте загрузки.',
         'BadUrl': 'Что-то не так, скачать не получается :(',
-        'SuccessfulSend': 'Наслаждайтесь. Отправить отзыв автору https://t.me/anton_pozdnyakov'
+        'SuccessfulSend': 'Наслаждайтесь. Отправить отзыв автору https://t.me/anton_pozdnyakov',
+        'Preparation': 'Подготовка'
     }
     return messages
